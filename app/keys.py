8f04154c07548eef1b5cc1c6fe269f0210cddf65
Andrew Amavizca <38573651+andrewamavizca@users.noreply.github.com>
2024-03-12 22:17:01 -0700

    Update keys.py

--- app/keys.py
+++ app/keys.py
@@ -1,7 +1,7 @@
-seller_id= 'A1Y7C41Y8GBAI6'
-access_key = 'AKIAIZRW2MM2XP4LB4RQ'
-secret_key ='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'
-marketplace_usa='ATVPDKIKX0DER'
-mws_auth_token = 'amzn.mws.92b0bd40-da22-40f0-eaaa-a12c640fe339'
+seller_id= '#########'
+access_key = '#########################'
+secret_key ='##################################'
+marketplace_usa='###################################'
+mws_auth_token = '########################################'
 
-access_id = 'PwVcpMByPcmnOiPSnhKIw'
+access_id = '###########################################'
